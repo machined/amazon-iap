da01c37431e5f5108db8ff129d6d610806506bc6
Max Maurer <maxemanuel.maurer@gmail.com>
2015-03-30 12:07:49 +0200

    implemented some starting point thing but need a sandbox server to test against

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var request = require('google-oauth-jwt').requestWithJWT();
+var request = require('request');
+var _ = require('lodash');
 var util = require('util');
 
 module.exports = Verifier;
@@ -7,24 +8,27 @@ function Verifier(options) {
 }
 
 Verifier.prototype.verify = function(receipt, cb) {
-  var urlPattern = "https://www.googleapis.com/androidpublisher/v2/applications/%s/purchases/subscriptions/%s/tokens/%s";
-  var finalUrl = util.format(urlPattern, encodeURIComponent(receipt.packageName), encodeURIComponent(receipt.productId), encodeURIComponent(receipt.purchaseToken));
+  var urlPattern = "https://appstore-sdk.amazon.com/version/1.0/verifyReceiptId/developer/%s/user/%s/receiptId/%s";
+  var finalUrl = util.format(urlPattern, encodeURIComponent(this.options.sharedKey), encodeURIComponent(receipt.userId), encodeURIComponent(receipt.receiptId));
   request({
     url: finalUrl,
-    jwt: {
-      email: this.options.email,
-      key: this.options.key,
-      keyFile: undefined,
-      scopes: ['https://www.googleapis.com/auth/androidpublisher']
-    }
-  }, function (err, res, body) {
+    json: true
+  }, function(err, res, body) {
     if (err) {
       return cb(err);
     }
-    var obj = JSON.parse(body);
-    if ("error" in obj) {
-      cb(new Error(obj.error.message));
-    } else if ("expiryTimeMillis" in obj && "startTimeMillis" in obj) {
+    if (res.statusCode === 400) {
+      return cb(new Error('Amazon RVS Error: Invalid receiptID'));
+    } else if (res.statusCode === 496) {
+      return cb(new Error('Amazon RVS Error: Invalid developerSecret'));
+    } else if (res.statusCode === 497) {
+      return cb(new Error('Amazon RVS Error: Invalid userId'));
+    } else if (res.statusCode === 500) {
+      return cb(new Error('Amazon RVS Error: Internal Server Error'));
+    } else if (res.statusCode !== 200) {
+      return cb(new Error('Amazon RVS Error: Unknown other error'));
+    }
+    if (_.every(['productId', 'productType', 'purchaseDate'], Object.prototype.hasOwnProperty, body)) {
       cb(null, obj);
     } else {
       cb(new Error("body did not contain expected json object"));
---
+++ spec/test.spec.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+var Verifier = require('../index');
+var sharedKey = '';
+var testData = {
+  "userId": "l3HL7XppEMhrOGDnur9-ulvqomrSg6qyODKmah76lJU=",
+  "sku": "de.test.subscription",
+  "purchaseDate": "Mon Mar 30 11:38:15 MESZ 2015",
+  "itemType": "SUBSCRIPTION",
+  "receiptId": "q1YqVrJSSknVK0ktLtErLk0qTi7KLCjJzM9T0lFKAUoZmhiZmxtYGFmaWppbAMVKgWI5xh4-5hEFBa6-GUX-7i55pUWWuqU5ZYX5uUXB6WaFlf4u3rmJGeZmOV6htkAtJUpWRrUA"
+};
+describe('verifier', function() {
+  it('successfully verifies a correct subscription receipt', function(done) {
+    var verifier = new Verifier({sharedKey: sharedKey});
+    verifier.verify(testData, function(err, result) {
+      expect(err).to.not.be.ok();
+      expect(result).to.be.ok();
+    });
+  });
+});
