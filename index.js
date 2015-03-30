4a7f5599ca0a7909d8fff1940f19014a7c7e2896
Max Maurer <maxemanuel.maurer@gmail.com>
2015-03-30 10:28:36 +0200

    initial commit (to some degree c&p from google play receipt verifier)

---
+++ index.js
@@ -0,0 +1,33 @@
+var request = require('google-oauth-jwt').requestWithJWT();
+var util = require('util');
+
+module.exports = Verifier;
+function Verifier(options) {
+  this.options = options || {};
+}
+
+Verifier.prototype.verify = function(receipt, cb) {
+  var urlPattern = "https://www.googleapis.com/androidpublisher/v2/applications/%s/purchases/subscriptions/%s/tokens/%s";
+  var finalUrl = util.format(urlPattern, encodeURIComponent(receipt.packageName), encodeURIComponent(receipt.productId), encodeURIComponent(receipt.purchaseToken));
+  request({
+    url: finalUrl,
+    jwt: {
+      email: this.options.email,
+      key: this.options.key,
+      keyFile: undefined,
+      scopes: ['https://www.googleapis.com/auth/androidpublisher']
+    }
+  }, function (err, res, body) {
+    if (err) {
+      return cb(err);
+    }
+    var obj = JSON.parse(body);
+    if ("error" in obj) {
+      cb(new Error(obj.error.message));
+    } else if ("expiryTimeMillis" in obj && "startTimeMillis" in obj) {
+      cb(null, obj);
+    } else {
+      cb(new Error("body did not contain expected json object"));
+    }
+  });
+};
