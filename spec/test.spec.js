6e02d00ae4f892ae48f65e558b00186563c8e164
Jiri Pospisil <mekishizufu@gmail.com>
2015-05-08 17:11:37 +0200

    For the sandbox env, sharedKey can be any non empty string

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var Verifier = require('../index');
-var sharedKey = '';
+
+var sharedKey = 'non empty';
 var testData = {
   "userId": "l3HL7XppEMhrOGDnur9-ulvqomrSg6qyODKmah76lJU=",
   "sku": "de.test.subscription",
@@ -8,6 +9,7 @@ var testData = {
   "itemType": "SUBSCRIPTION",
   "receiptId": "q1YqVrJSSknVK0ktLtErLk0qTi7KLCjJzM9T0lFKAUoZmhiZmxtYGFmaWppbAMVKgWI5xh4-5hEFBa6-GUX-7i55pUWWuqU5ZYX5uUXB6WaFlf4u3rmJGeZmOV6htkAtJUpWRrUA"
 };
+
 describe('verifier', function() {
   it('successfully verifies a correct subscription receipt', function(done) {
     var verifier = new Verifier({sharedKey: sharedKey});
