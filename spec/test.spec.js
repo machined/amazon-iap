16703d812a3219fdf8a645aa11896fbc14de3d5a
Jiri Pospisil <mekishizufu@gmail.com>
2015-05-08 17:02:21 +0200

    Do not forget to call the done callback

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -14,6 +14,7 @@ describe('verifier', function() {
     verifier.verify(testData, function(err, result) {
       expect(err).to.not.be.ok;
       expect(result).to.be.ok;
+      done();
     });
   });
 });
